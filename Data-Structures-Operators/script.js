005eface5047d6888973dd845458d5074a439e68
tristandavoulas <tdavoulas@protonmail.com>
2022-12-12 14:40:07 -0800

    Challenge #2 Part 1 Complete

--- Data-Structures-Operators/script.js
+++ Data-Structures-Operators/script.js
@@ -88,3 +88,39 @@ const game = {
   // Let's use || operator
   team1 < team2 && console.log(`Team 1 is likely to win!`);
   team2 < team1 && console.log('Team 2 is likely to win!');
+
+  ///////////////////////////////////////
+// Coding Challenge #2
+
+/* 
+Let's continue with our football betting app!
+
+1. Loop over the game.scored array and print each player name to the console, along with the goal number (Example: "Goal 1: Lewandowski")
+2. Use a loop to calculate the average odd and log it to the console (We already studied how to calculate averages, you can go check if you don't remember)
+3. Print the 3 odds to the console, but in a nice formatted way, exaclty like this:
+      Odd of victory Bayern Munich: 1.33
+      Odd of draw: 3.25
+      Odd of victory Borrussia Dortmund: 6.5
+Get the team names directly from the game object, don't hardcode them (except for "draw"). HINT: Note how the odds and the game objects have the same property names 😉
+
+BONUS: Create an object called 'scorers' which contains the names of the players who scored as properties, and the number of goals as the value. In this game, it will look like this:
+      {
+        Gnarby: 1,
+        Hummels: 1,
+        Lewandowski: 2
+      }
+
+GOOD LUCK 😀
+*/
+
+// Part 1 - Using old skool for loop
+// for (let i = 0; i < game.scored.length; i++) {
+//   console.log(i)
+// }
+
+// Part 1 - Using for-of loop
+for (const [i, playerName] of game.scored.entries()) {
+  console.log(`Goal ${i + 1}: ${playerName}`);
+}
+
+
