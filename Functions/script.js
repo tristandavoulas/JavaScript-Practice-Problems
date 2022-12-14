96479b67f5121958d25da552bea3984e87486699
tristandavoulas <52335483+tristandavoulas@users.noreply.github.com>
2022-12-13 21:30:59 -0800

    Adjust button for small windows, implement click f(x)

--- Functions/script.js
+++ Functions/script.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const poll = {
+    registerNewAnswer() {
+        console.log(`What is your favorite programming language?\n0: JavaScript\n1: Python\n2: Rust\n3: C++\n(Write option number)`);
+    }
+};
+
+document.querySelector(".poll").addEventListener('click', poll.registerNewAnswer);
