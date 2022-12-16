7bac25337c3edd29b82b170ee2b012fe4ed70d6f
tristandavoulas <52335483+tristandavoulas@users.noreply.github.com>
2022-12-15 21:29:10 -0800

    Implement answer button functionality

--- Functions/script.js
+++ Functions/script.js
@@ -1,9 +1,50 @@
 'use strict';
 
-const poll = {
+let poll = {
+    question: `What is your favorite programming language?`,
+    // answers: {
+    //     JavaScript: 0,
+    //     Python: 0,
+    //     Rust: 0,
+    //     C: 0
+    // },
+
+    answers: [
+        `JavaScript`,
+        `Python`,
+        `Rust`,
+        `C++`
+    ],
+
+    values: [
+        0,
+        0,
+        0,
+        0,
+    ],
+
     registerNewAnswer() {
-        console.log(`What is your favorite programming language?\n0: JavaScript\n1: Python\n2: Rust\n3: C++\n(Write option number)`);
-    }
+        // console.log(this);
+        console.log(poll.question);
+        for (const [i, answer] of Object.entries(poll.answers)) {
+            console.log(`${i}: ${answer}`);
+        }
+        console.log(`Write option number`);
+        let input = prompt('Input a number:');
+        while (input < 0 || input > 3) {
+            input = prompt('Input out of range: try again');
+        }
+        poll.values[input]++;
+        poll.displayResults('string');
+    },
+
+    displayResults(type) {
+        if (type === 'array') {
+            console.log(poll.values);
+        } else if (type === 'string') {
+            console.log(`Poll results are ${this.values.join(', ')}`)
+        }
+    },
 };
 
 document.querySelector(".poll").addEventListener('click', poll.registerNewAnswer);
